39089531984d3b096e4ad7cc509898fea5a2e86e
David Browning <david.a.browning@gmail.com>
2025-01-24 18:06:43 +0100

    Solve Weird String Case kata.

---
+++ WeirdStringCase/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace WeirdStringCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Weird string case";
+            Console.WriteLine($"{s} => {Kata.ToWeirdCase(s)}");
+        }
+    }
+
+    public class Kata
+    {
+        public static string ToWeirdCase(string s)
+        {
+            string result = "";
+            int letterCounter = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    letterCounter = 0;
+                    result += s[i];
+                }
+                else if (letterCounter % 2 == 0)
+                {
+                    letterCounter++;
+                    result += s[i].ToString().ToUpper();
+                }
+                else
+                {
+                    letterCounter++;
+                    result += s[i].ToString().ToLower();
+                }
+            }
+            return result;
+        }
+    }
+}
