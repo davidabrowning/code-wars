586de7b6c2c6f84accc38d03c47c1ad233925029
David Browning <david.a.browning@gmail.com>
2025-01-25 19:45:37 +0100

    Complete PileOfCubes kata.

---
+++ PileOfCubes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PileOfCubes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] inputs = { 1071225, 91716553919377 };
+            long[] answers =
+            {
+                findNb(inputs[0]),
+                findNb(inputs[1])
+            };
+
+            Console.WriteLine($"findNb({inputs[0]})-->{answers[0]}");
+            Console.WriteLine($"findNb({inputs[1]})-->{answers[1]}");
+        }
+
+        public static long findNb(long m)
+        {
+            int cubesOnThisLevel = 0;
+            long area = 0;
+            long targetArea = m;
+            while (area < targetArea)
+            {
+                cubesOnThisLevel++;
+                area += (long) Math.Pow(cubesOnThisLevel, 3);
+            }
+            if (area == targetArea)
+            {
+                return cubesOnThisLevel;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
